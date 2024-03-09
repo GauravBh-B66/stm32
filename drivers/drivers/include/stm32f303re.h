e2ace12b2c2e47db7277071c7a144fdef5fba961
GauravBh-B66 <g.bhattarai1202@gmail.com>
2024-03-09 20:52:54 +0545

    Device header file.

---
+++ drivers/drivers/include/stm32f303re.h
@@ -0,0 +1,22 @@
+/*
+ * stm32f303re.h
+ *
+ *  Created on: Mar 9, 2024
+ *      Author: user_gs
+ */
+
+#ifndef INCLUDE_STM32F303RE_H_
+#define INCLUDE_STM32F303RE_H_
+
+//This header file contains microcontroller specific details which includes
+// base address of memory devices like flash, sram, etc
+// base address of buses
+// base address of peripherals in different buses
+// clock management macros
+// irq definitions
+// peripheral registers details
+// other configuration macros
+
+
+
+#endif /* INCLUDE_STM32F303RE_H_ */
