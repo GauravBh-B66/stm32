332004315c799ce074b01055669256268ee40a79
GauravBh-B66 <g.bhattarai1202@gmail.com>
2024-03-10 17:30:47 +0545

    Memory and Bus address

---
+++ stm32f303re.h
@@ -0,0 +1,10 @@
+//This header file contains microcontroller specific details which includes
+
+base address of memory devices like flash, sram, etc
+base address of buses
+base address of peripherals in different buses
+clock management macros
+irq definitions
+peripheral registers details
+other configuration macros
+
